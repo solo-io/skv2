896f8310a07b1da1d19f05c25459e598b19af610
EItanya <eitan.yarmush@solo.io>
2020-04-20 15:31:43 -0400

    got first versions

---
+++ pkg/generated/generate.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"log"
+	"path/filepath"
+
+	"github.com/solo-io/skv2/codegen"
+	"github.com/solo-io/skv2/codegen/model"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+const (
+	generatedPackageName = "pkg/generated"
+)
+
+var (
+	kubeGeneratedPackage = filepath.Join(generatedPackageName, "kubernetes")
+)
+
+func main() {
+	log.Println("starting kube client generation")
+	skv2Cmd := codegen.Command{
+		Groups: []model.Group{
+			{
+				GroupVersion: schema.GroupVersion{
+					Group:   "core",
+					Version: "v1",
+				},
+				Module: "k8s.io/api",
+				Resources: []model.Resource{
+					{
+						Kind: "Secret",
+					},
+					{
+						Kind: "ServiceAccount",
+					},
+					{
+						Kind: "ConfigMap",
+					},
+					{
+						Kind: "Service",
+					},
+					{
+						Kind: "Pod",
+					},
+					{
+						Kind: "Namespace",
+					},
+					{
+						Kind: "Node",
+
+					},
+				},
+				RenderController: true,
+				RenderClients:    true,
+				CustomTypesImportPath: "k8s.io/api/core/v1",
+				ApiRoot:               kubeGeneratedPackage,
+			},
+			{
+				GroupVersion: schema.GroupVersion{
+					Group:   "apps",
+					Version: "v1",
+				},
+				Module: "k8s.io/api",
+				Resources: []model.Resource{
+					{
+						Kind: "Deployment",
+					},
+					{
+						Kind: "ReplicaSet",
+					},
+				},
+				RenderController: true,
+				RenderClients:    true,
+				CustomTypesImportPath: "k8s.io/api/apps/v1",
+				ApiRoot:               kubeGeneratedPackage,
+			},
+			{
+				GroupVersion: schema.GroupVersion{
+					Group:   "admissionregistration.k8s.io",
+					Version: "v1",
+				},
+				Module: "k8s.io/apiextensions-apiserver",
+				Resources: []model.Resource{
+					{
+						Kind: "ValidatingWebhookConfiguration",
+					},
+				},
+				RenderClients: true,
+				CustomTypesImportPath: "k8s.io/api/admissionregistration/v1",
+				ApiRoot:               kubeGeneratedPackage,
+			},
+			{
+				GroupVersion: schema.GroupVersion{
+					Group:   "apiextensions.k8s.io",
+					Version: "v1beta1",
+				},
+				Module: "k8s.io/apiextensions-apiserver",
+				Resources: []model.Resource{
+					{
+						Kind: "CustomResourceDefinition",
+					},
+				},
+				RenderClients: true,
+				CustomTypesImportPath: "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1",
+				ApiRoot:               kubeGeneratedPackage,
+			},
+		},
+	}
+	if err := skv2Cmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Finished generating kube clients\n")
+}
