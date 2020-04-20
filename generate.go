16bbf10806b5821dbaefea64dccbe78fb4757bcc
Joe Kelley <jsphklly@gmail.com>
2020-04-20 13:43:55 -0400

    cleanup generate.go

--- generate.go
+++ generate.go
@@ -5,29 +5,14 @@ import (
 
 	"github.com/solo-io/skv2/codegen"
 	"github.com/solo-io/skv2/codegen/model"
-	"github.com/solo-io/solo-kit/pkg/code-generator/sk_anyvendor"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 //go:generate go run generate.go
 
 func main() {
-	protoImports := sk_anyvendor.CreateDefaultMatchOptions([]string{
-		"api/**/*.proto",
-		"pkg/**/*.proto",
-	})
-
-	// custom proto imports
-	protoImports.External["k8s.io/api"] = []string{
-		"core/v1/*.proto",
-	}
-	protoImports.External["k8s.io/apimachinery"] = []string{
-		"pkg/**/*.proto",
-	}
-
 	cmd := &codegen.Command{
-		AppName:         "dev-portal",
-		AnyVendorConfig: protoImports,
+		AppName: "skv2",
 		Groups: []model.Group{
 			{
 				GroupVersion: schema.GroupVersion{
