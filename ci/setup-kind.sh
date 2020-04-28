6b6e2fabacb8eb20adb5b08a422f13f339580614
Joe Kelley <jsphklly@gmail.com>
2020-04-28 11:02:37 -0400

    less magic'

--- ci/setup-kind.sh
+++ ci/setup-kind.sh
@@ -2,13 +2,12 @@
 
 set -ex
 
-if [ "$1" == "cleanup" ]; then
-  kind delete cluster --name skv2-test-master
-  kind delete cluster --name skv2-test-remote
+if [ "$1" == "" ]; then
+  echo "please provide a name for the 'remote' cluster"
   exit 0
 fi
 
 kind create cluster --name skv2-test-master
-kind create cluster --name skv2-test-remote
+kind create cluster --name "$1"
 
 kubectl config use-context kind-skv2-test-master
