95c118cd5f480088b8e9f82b353001a8aae4e513
EItanya <eitan.yarmush@solo.io>
2020-05-06 12:32:09 -0400

    added kube cluster API

---
+++ api/generate.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+
+	"github.com/solo-io/skv2/codegen"
+	"github.com/solo-io/skv2/codegen/model"
+	"github.com/solo-io/solo-kit/pkg/code-generator/sk_anyvendor"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+//go:generate go run generate.go
+
+func main() {
+	log.Println("starting kube client generation")
+
+	skv2Imports := sk_anyvendor.CreateDefaultMatchOptions([]string{
+		"api/**/*.proto",
+	})
+
+	skv2Cmd := codegen.Command{
+		Groups: []model.Group{
+			{
+				GroupVersion: schema.GroupVersion{
+					Group:   "multicluster.solo.io",
+					Version: "v1alpha1",
+				},
+				Module: "github.com/solo-io/skv2",
+				Resources: []model.Resource{
+					{
+						Kind: "KubernetesCluster",
+						Spec: model.Field{
+							Type: model.Type{
+								Name:      "KubernetesClusterSpec",
+								GoPackage: "github.com/solo-io/skv2/pkg/api/multicluster.solo.io/v1alpha1/types",
+							},
+						},
+						Status: &model.Field{
+							Type: model.Type{
+								Name:      "KubernetesClusterStatus",
+								GoPackage: "github.com/solo-io/skv2/pkg/api/multicluster.solo.io/v1alpha1/types",
+							},
+						},
+					},
+				},
+				RenderManifests:  true,
+				RenderController: true,
+				RenderProtos:     true,
+				RenderClients:    true,
+				RenderTypes:      true,
+				ApiRoot:          "pkg/api",
+			},
+		},
+		AnyVendorConfig: skv2Imports,
+	}
+	if err := skv2Cmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Finished generating kube clients\n")
+}
