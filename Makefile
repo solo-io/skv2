129570e801082291938f1f7424054cddb5803562
EItanya <eitan.yarmush@solo.io>
2020-05-18 20:46:42 -0400

    fixed ordering and added secret_key

--- Makefile
+++ Makefile
@@ -17,13 +17,13 @@ install-deps: mod-download
 	go get -v github.com/solo-io/protoc-gen-ext
 	go get -v github.com/golang/mock/mockgen
 	go get -v golang.org/x/tools/cmd/goimports@v0.0.0-20200427205912-352a5409fae0
-	go mod tidy
 
 # Generated Code - Required to update Codgen Templates
 .PHONY: generated-code
 generated-code: clean install-deps
 	go generate ./...
 	goimports -w .
+	go mod tidy
 
 #----------------------------------------------------------------------------------
 # Test
--- pkg/api/multicluster.solo.io/v1alpha1/cluster.pb.hash.go
+++ pkg/api/multicluster.solo.io/v1alpha1/cluster.pb.hash.go
@@ -38,18 +38,12 @@ func (m *KubernetesClusterSpec) Hash(hasher hash.Hash64) (uint64, error) {
 		return 0, err
 	}
 
-	if h, ok := interface{}(m.GetSecret()).(safe_hasher.SafeHasher); ok {
-		if _, err = h.Hash(hasher); err != nil {
-			return 0, err
-		}
-	} else {
-		if val, err := hashstructure.Hash(m.GetSecret(), nil); err != nil {
-			return 0, err
-		} else {
-			if err := binary.Write(hasher, binary.LittleEndian, val); err != nil {
-				return 0, err
-			}
-		}
+	if _, err = hasher.Write([]byte(m.GetSecretName())); err != nil {
+		return 0, err
+	}
+
+	if _, err = hasher.Write([]byte(m.GetSecretKey())); err != nil {
+		return 0, err
 	}
 
 	return hasher.Sum64(), nil
