e49cf52095bd9fcc4e394dfdc2a3d74664ec9b15
Scott Weiss <sdw35@cornell.edu>
2020-07-07 10:38:02 -0400

    move object ref funcs

--- pkg/ezkube/object_ref.go
+++ pkg/ezkube/object_ref.go
@@ -1,18 +1,17 @@
-package refutils
+package ezkube
 
 import (
 	v1 "github.com/solo-io/skv2/pkg/api/core.skv2.solo.io/v1"
-	"github.com/solo-io/skv2/pkg/ezkube"
 )
 
-func MakeObjectRef(resource ezkube.ResourceId) *v1.ObjectRef {
+func MakeObjectRef(resource ResourceId) *v1.ObjectRef {
 	return &v1.ObjectRef{
 		Name:      resource.GetName(),
 		Namespace: resource.GetNamespace(),
 	}
 }
 
-func MakeClusterObjectRef(resource ezkube.ClusterResourceId) *v1.ClusterObjectRef {
+func MakeClusterObjectRef(resource ClusterResourceId) *v1.ClusterObjectRef {
 	return &v1.ClusterObjectRef{
 		Name:        resource.GetName(),
 		Namespace:   resource.GetNamespace(),
