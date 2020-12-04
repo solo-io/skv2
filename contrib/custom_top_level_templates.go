bb3e7307eb552f00a8a08c2f8f71b01838dccf6b
Scott Weiss <sdw35@cornell.edu>
2020-12-04 16:19:40 -0600

    support hybrid input reconciler for reconciliation of events that happen on both local and managed clusters

--- contrib/custom_top_level_templates.go
+++ contrib/custom_top_level_templates.go
@@ -4,6 +4,7 @@ import (
 	"github.com/solo-io/skv2/codegen/model"
 	"github.com/solo-io/skv2/contrib/codegen/funcs"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"text/template"
 )
 
 /*
@@ -11,29 +12,70 @@ Define custom templates that live on the group level here.
 */
 
 // Parameters for constructing templates that span across multiple Groups.
-type CrossGroupTemplateParameters struct {
+type SnapshotTemplateParameters struct {
 	// the path of the output file produced from this template. relative to project root.
 	OutputFilename string
 
 	// a map of Go modules to (a superset of) the imported codegen Groups. only required if the codegen group is defined in a different go module than the types (i.e. it is using a CustomTypesImportPath)
 	SelectFromGroups map[string][]model.Group
 
+	SnapshotResources SnapshotResources
+}
+
+// SnapshotResources acts as a "oneof" to encapsulate
+// HybridSnapshot or a HomogenousSnapshot
+type SnapshotResources interface {
+	makeTemplateFuncs(outputFilename string, selectFromGroups map[string][]model.Group) template.FuncMap
+}
+
+// HomogenousSnapshotResources represents a set of snapshot resources read from a single source (either remote clusters or local cluster)
+type HomogenousSnapshotResources struct {
 	// a map of the GVKs to the resources which we want to include in the input snapshot.
 	ResourcesToSelect map[schema.GroupVersion][]string
 }
 
-func (p CrossGroupTemplateParameters) constructTemplate(params CrossGroupTemplateParameters, templatePath string) model.CustomTemplates {
+func (r HomogenousSnapshotResources) makeTemplateFuncs(outputFilename string, selectFromGroups map[string][]model.Group) template.FuncMap {
+	return funcs.MakeHomogenousSnapshotFuncs(
+		outputFilename,
+		selectFromGroups,
+		r.ResourcesToSelect,
+	)
+}
+
+// HybridSnapshotResources represents a set of snapshot resources read from a both source remote clusters and the local cluster
+type HybridSnapshotResources struct {
+	// a map of the GVKs to the resources which we want to include in the input snapshot.
+	// these resourecs will be stored in the the local cluster where the controller is running.
+	LocalResourcesToSelect map[schema.GroupVersion][]string
+
+	// a map of the GVKs to the resources which we want to include in the input snapshot.
+	// these resourecs will be stored in the the remote cluster which the controller manages.
+	RemoteResourcesToSelect map[schema.GroupVersion][]string
+}
+
+func (r HybridSnapshotResources) makeTemplateFuncs(outputFilename string,selectFromGroups map[string][]model.Group) template.FuncMap {
+	return funcs.MakeHybridSnapshotFuncs(
+		outputFilename,
+		selectFromGroups,
+		r.LocalResourcesToSelect,
+		r.RemoteResourcesToSelect,
+	)
+}
+
+
+func (p SnapshotTemplateParameters) constructTemplate(params SnapshotTemplateParameters, templatePath string) model.CustomTemplates {
 	templateContents, err := templatesBox.FindString(templatePath)
 	if err != nil {
 		panic(err)
 	}
-	inputSnapshotTemplate := model.CustomTemplates{
+
+	crossGroupTemplate := model.CustomTemplates{
 		Templates:        map[string]string{params.OutputFilename: templateContents},
 		MockgenDirective: true,
-		Funcs:            funcs.MakeTopLevelFuncs(params.OutputFilename, params.SelectFromGroups, params.ResourcesToSelect),
+		Funcs:            p.SnapshotResources.makeTemplateFuncs(p.OutputFilename, p.SelectFromGroups),
 	}
 
-	return inputSnapshotTemplate
+	return crossGroupTemplate
 }
 
 // NOTE(ilackarms): to add your template, copy paste XXX custom template below
@@ -46,7 +88,7 @@ const (
 )
 
 // Returns the template for generating input snapshots.
-func InputSnapshot(params CrossGroupTemplateParameters) model.CustomTemplates {
+func InputSnapshot(params SnapshotTemplateParameters) model.CustomTemplates {
 	return params.constructTemplate(params, InputSnapshotCustomTemplatePath)
 }
 
@@ -58,20 +100,25 @@ const (
 )
 
 // Returns the template for generating input snapshots.
-func InputSnapshotManualBuilder(params CrossGroupTemplateParameters) model.CustomTemplates {
+func InputSnapshotManualBuilder(params SnapshotTemplateParameters) model.CustomTemplates {
 	return params.constructTemplate(params, InputSnapshotManualBuilderCustomTemplatePath)
 }
 
 /*
-InputReconciler custom template
+InputReconciler custom templates
 */
 const (
-	InputReconcilerCustomTemplatePath = "input/input_reconciler.gotmpl"
+	HomogenousInputReconcilerCustomTemplatePath = "input/input_reconciler.gotmpl"
+	HybridInputReconcilerCustomTemplatePath = "input/hybrid_input_reconciler.gotmpl"
 )
 
 // Returns the template for generating input reconcilers.
-func InputReconciler(params CrossGroupTemplateParameters) model.CustomTemplates {
-	return params.constructTemplate(params, InputReconcilerCustomTemplatePath)
+func InputReconciler(params SnapshotTemplateParameters) model.CustomTemplates {
+	templatePath := HomogenousInputReconcilerCustomTemplatePath
+	if _, isHybrid := params.SnapshotResources.(HybridSnapshotResources); isHybrid {
+		templatePath = HybridInputReconcilerCustomTemplatePath
+	}
+	return params.constructTemplate(params, templatePath)
 }
 
 /*
@@ -82,6 +129,6 @@ const (
 )
 
 // Returns the template for generating output snapshots.
-func OutputSnapshot(params CrossGroupTemplateParameters) model.CustomTemplates {
+func OutputSnapshot(params SnapshotTemplateParameters) model.CustomTemplates {
 	return params.constructTemplate(params, OutputSnapshotCustomTemplatePath)
 }
--- contrib/pkg/input/input_reconciler.go
+++ contrib/pkg/input/input_reconciler.go
@@ -20,22 +20,19 @@ import (
 // the passed resource can either be a ref to a resource (caused by a deletion), or an actual resource itself.
 type SingleClusterReconcileFunc func(id ezkube.ResourceId) (bool, error)
 
-// the SingleClusterReconciler reconciles events for input resources in a single cluster
-type SingleClusterReconciler interface {
-	// reconcile the generic resource type.
-	// this function is called from generated code.
-	ReconcileGeneric(id ezkube.ResourceId) (reconcile.Result, error)
-}
-
 // reconcile a resource across multiple clusters.
 // the passed resource can either be a ref to a resource (caused by a deletion), or an actual resource itself. ClusterName will always be set on the object.
 type MultiClusterReconcileFunc func(id ezkube.ClusterResourceId) (bool, error)
 
-// the MultiClusterReconciler reconciles events for input resources across clusters
-type MultiClusterReconciler interface {
-	// reconcile the generic resource type.
+// the SingleClusterReconciler reconciles events for input resources in a single cluster
+type InputReconciler interface {
+	// reconcile the generic resource type in the local cluster.
 	// this function is called from generated code.
-	ReconcileClusterGeneric(id ezkube.ClusterResourceId) (reconcile.Result, error)
+	ReconcileLocalGeneric(id ezkube.ResourceId) (reconcile.Result, error)
+
+	// reconcile the generic resource type in a remote cluster.
+	// this function is called from generated code.
+	ReconcileRemoteGeneric(id ezkube.ClusterResourceId) (reconcile.Result, error)
 }
 
 // input reconciler implements both the single and multi cluster reconcilers, for convenience.
@@ -50,41 +47,28 @@ type inputReconciler struct {
 // also starts the reconciler's event processor in a goroutine.
 // Make sure to cancel the parent context in order to ensure the goroutine started here is gc'ed.
 // only one event will be processed per reconcileInterval.
-func NewMultiClusterReconcilerImpl(
+func NewInputReconciler(
 	ctx context.Context,
-	reconcileFunc MultiClusterReconcileFunc,
+	multiClusterReconcileFunc MultiClusterReconcileFunc,
+	singleClusterReconcileFunc SingleClusterReconcileFunc,
 	reconcileInterval time.Duration,
-) MultiClusterReconciler {
+) InputReconciler {
 	r := &inputReconciler{
 		ctx:                       ctx,
 		queue:                     workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter()),
-		multiClusterReconcileFunc: reconcileFunc,
+		multiClusterReconcileFunc: multiClusterReconcileFunc,
+		singleClusterReconcileFunc: singleClusterReconcileFunc,
 	}
 	go r.reconcileEventsForever(reconcileInterval)
 	return r
 }
 
-// Note(ilackarms): in the current implementation, the constructor
-// also starts the reconciler's event processor in a goroutine.
-// Make sure to cancel the parent context in order to ensure the goroutine started here is gc'ed.
-// only one event will be processed per reconcileInterval.
-func NewSingleClusterReconciler(
-	ctx context.Context,
-	reconcileFunc SingleClusterReconcileFunc,
-	reconcileInterval time.Duration,
-) SingleClusterReconciler {
-	r := &inputReconciler{
-		ctx:                        ctx,
-		queue:                      workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter()),
-		singleClusterReconcileFunc: reconcileFunc,
-	}
-	go r.reconcileEventsForever(reconcileInterval)
-	return r
-}
-
-func (r *inputReconciler) ReconcileGeneric(id ezkube.ResourceId) (reconcile.Result, error) {
+func (r *inputReconciler) ReconcileLocalGeneric(id ezkube.ResourceId) (reconcile.Result, error) {
 	if r.ctx == nil {
 		return reconcile.Result{}, eris.Errorf("internal error: reconciler not started")
+	}
+	if r.singleClusterReconcileFunc == nil {
+		return reconcile.Result{}, eris.Errorf("internal error: no single-cluster reconcile func provided; cannot reconcile %v", sets.Key(id))
 	}
 	// never queue more than one event
 	if r.queue.Len() < 2 {
@@ -95,9 +79,12 @@ func (r *inputReconciler) ReconcileGeneric(id ezkube.ResourceId) (reconcile.Resu
 	return reconcile.Result{}, nil
 }
 
-func (r *inputReconciler) ReconcileClusterGeneric(id ezkube.ClusterResourceId) (reconcile.Result, error) {
+func (r *inputReconciler) ReconcileRemoteGeneric(id ezkube.ClusterResourceId) (reconcile.Result, error) {
 	if r.ctx == nil {
 		return reconcile.Result{}, eris.Errorf("internal error: reconciler not started")
+	}
+	if r.multiClusterReconcileFunc == nil {
+		return reconcile.Result{}, eris.Errorf("internal error: no multi-cluster reconcile func provided; cannot reconcile %v", sets.Key(id))
 	}
 	// never queue more than one event
 	if r.queue.Len() < 2 {
@@ -129,15 +116,24 @@ func (r *inputReconciler) processNextWorkItem() bool {
 	}
 	defer r.queue.Done(key)
 
+	var isRemoteCluster bool
+
+	// determine whether the resource has been read from a remote cluster
+	// based on whether its ClusterName field is set
+	if  clusterResource, ok := key.(ezkube.ClusterResourceId); ok {
+		if clusterResource.GetClusterName() != "" {
+			isRemoteCluster = true
+		}
+	}
+
 	var (
 		requeue bool
 		err     error
 	)
-	switch {
-	case r.singleClusterReconcileFunc != nil:
-		requeue, err = r.singleClusterReconcileFunc(key.(ezkube.ResourceId))
-	case r.multiClusterReconcileFunc != nil:
+	if isRemoteCluster {
 		requeue, err = r.multiClusterReconcileFunc(key.(ezkube.ClusterResourceId))
+	} else {
+		requeue, err = r.singleClusterReconcileFunc(key.(ezkube.ResourceId))
 	}
 
 	switch {
