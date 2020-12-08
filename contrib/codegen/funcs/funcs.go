8f4ddd6e5328cf1b7bc3a7ccae39c0714b8d0770
Scott Weiss <scott.weiss@solo.io>
2020-12-08 14:28:22 -0600

    Hybrid snapshot (#179)
    * support hybrid input reconciler for reconciliation of events that happen on both local and managed clusters
    * Merge branch 'master' into hybrid-snapshot
    * fmt
    * fix homogenous reconcilers
    * Merge branch 'master' into hybrid-snapshot
    * support snapshot name
    * fix homogenous templates to only build single/multi cluster snapshot
    * fmt

--- contrib/codegen/funcs/funcs.go
+++ contrib/codegen/funcs/funcs.go
@@ -20,7 +20,47 @@ type importedGroup struct {
 //
 // selectFromGroups = a map of Go modules to (a superset of) the imported codegen Groups. only required if the codegen group is defined in a different go module than the types (i.e. it is using a CustomTypesImportPath)
 // resourcesToSelect = the GVKs of the resources which we want to select from the provided groups
-func MakeTopLevelFuncs(outputFile string, selectFromGroups map[string][]model.Group, resourcesToSelect map[schema.GroupVersion][]string) template.FuncMap {
+func MakeHomogenousSnapshotFuncs(
+	snapshotName, outputFile string,
+	selectFromGroups map[string][]model.Group,
+	resourcesToSelect map[schema.GroupVersion][]string,
+	multiCluster bool,
+) template.FuncMap {
+	groups, groupImports := getImportedGroups(selectFromGroups, resourcesToSelect)
+
+	return template.FuncMap{
+		"snapshot_name":   func() string { return snapshotName },
+		"is_multicluster": func() bool { return multiCluster },
+		"package": func() string {
+			dirs := strings.Split(filepath.Dir(outputFile), string(filepath.Separator))
+			return dirs[len(dirs)-1] // last path element = package name
+		},
+		"imported_groups": func() []model.Group { return groups },
+		"client_import_path": func(group model.Group) string {
+			grp, ok := groupImports[group.GroupVersion]
+			if !ok {
+				panic("group not found " + grp.String())
+			}
+			return clientImportPath(grp)
+		},
+		"set_import_path": func(group model.Group) string {
+			grp, ok := groupImports[group.GroupVersion]
+			if !ok {
+				panic("group not found " + grp.String())
+			}
+			return clientImportPath(grp) + "/sets"
+		},
+		"controller_import_path": func(group model.Group) string {
+			grp, ok := groupImports[group.GroupVersion]
+			if !ok {
+				panic("group not found " + grp.String())
+			}
+			return clientImportPath(grp) + "/controller"
+		},
+	}
+}
+
+func getImportedGroups(selectFromGroups map[string][]model.Group, resourcesToSelect map[schema.GroupVersion][]string) ([]model.Group, map[schema.GroupVersion]importedGroup) {
 	importedGroups := selectResources(selectFromGroups, resourcesToSelect)
 	var groups []model.Group
 	groupImports := map[schema.GroupVersion]importedGroup{}
@@ -31,12 +71,42 @@ func MakeTopLevelFuncs(outputFile string, selectFromGroups map[string][]model.Gr
 		groupImports[grp.GroupVersion] = grp
 	}
 
+	return groups, groupImports
+}
+
+// make funcs for "Top Level" templates, i.e.: templates which
+// combine resources from multiple (including externally defined) codegen Groups.
+//
+// selectFromGroups = a map of Go modules to (a superset of) the imported codegen Groups. only required if the codegen group is defined in a different go module than the types (i.e. it is using a CustomTypesImportPath)
+// resourcesToSelect = the GVKs of the resources which we want to select from the provided groups
+func MakeHybridSnapshotFuncs(
+	snapshotName, outputFile string,
+	selectFromGroups map[string][]model.Group,
+	localResourcesToSelect, remoteResourcesToSelect map[schema.GroupVersion][]string,
+) template.FuncMap {
+	localGroups, localGroupImports := getImportedGroups(selectFromGroups, localResourcesToSelect)
+	remoteGroups, remoteGroupImports := getImportedGroups(selectFromGroups, remoteResourcesToSelect)
+
+	groups := append([]model.Group{}, localGroups...)
+	groups = append([]model.Group{}, remoteGroups...)
+
+	groupImports := map[schema.GroupVersion]importedGroup{}
+	for groupVersion, group := range localGroupImports {
+		groupImports[groupVersion] = group
+	}
+	for groupVersion, group := range remoteGroupImports {
+		groupImports[groupVersion] = group
+	}
+
 	return template.FuncMap{
+		"snapshot_name": func() string { return snapshotName },
 		"package": func() string {
 			dirs := strings.Split(filepath.Dir(outputFile), string(filepath.Separator))
 			return dirs[len(dirs)-1] // last path element = package name
 		},
-		"imported_groups": func() []model.Group { return groups },
+		"imported_groups":        func() []model.Group { return groups },
+		"local_imported_groups":  func() []model.Group { return localGroups },
+		"remote_imported_groups": func() []model.Group { return remoteGroups },
 		"client_import_path": func(group model.Group) string {
 			grp, ok := groupImports[group.GroupVersion]
 			if !ok {
