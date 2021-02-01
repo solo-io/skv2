10e3cbbbc1109a63d75b0cb2a5f139fb8f84df81
Kevin Dorosh <kcdorosh@gmail.com>
2021-02-01 13:49:13 -0500

    Fix equality

--- pkg/controllerutils/equality_test.go
+++ pkg/controllerutils/equality_test.go
@@ -5,6 +5,7 @@ import (
 	. "github.com/onsi/gomega"
 	things_test_io_v1 "github.com/solo-io/skv2/codegen/test/api/things.test.io/v1"
 	v1 "k8s.io/api/core/v1"
+	rbac_authorization_k8s_io_v1 "k8s.io/api/rbac/v1"
 	k8s_meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	. "github.com/solo-io/skv2/pkg/controllerutils"
@@ -81,8 +82,7 @@ var _ = Describe("ObjectsEqual", func() {
 		equal := ObjectsEqual(obj1, obj2)
 		Expect(equal).To(BeFalse())
 	})
-
-	It("asserts equality on two proto.Message objects even if they are passed in by value", func() {
+	It("asserts equality on two proto.Message objects (protov2, with unknown fields) even if they are passed in by reference", func() {
 		obj1 := &things_test_io_v1.Paint{
 			Spec: things_test_io_v1.PaintSpec{
 				Color: &things_test_io_v1.PaintColor{
@@ -102,6 +102,46 @@ var _ = Describe("ObjectsEqual", func() {
 				PaintType: &things_test_io_v1.PaintSpec_Oil{
 					Oil: nil,
 				},
+			},
+		}
+		equal := ObjectsEqual(obj1, obj2)
+		Expect(equal).To(BeTrue())
+	})
+	It("asserts equality on two proto.Message objects even if they are 'fake' (i.e., k8s protos implementing github protov1, not google protov2 protos)", func() {
+		obj1 := &rbac_authorization_k8s_io_v1.RoleBinding{
+			TypeMeta: k8s_meta.TypeMeta{},
+			ObjectMeta: k8s_meta.ObjectMeta{
+				Name:      "sample-role-binding",
+				Namespace: "default",
+				Labels:    map[string]string{"k": "v"},
+			},
+			Subjects: []rbac_authorization_k8s_io_v1.Subject{{
+				Kind:      "ServiceAccount",
+				Name:      "sample-target",
+				Namespace: "default",
+			}},
+			RoleRef: rbac_authorization_k8s_io_v1.RoleRef{
+				APIGroup: "rbac.authorization.k8s.io",
+				Kind:     "Role",
+				Name:     "sample-controller",
+			},
+		}
+		obj2 := &rbac_authorization_k8s_io_v1.RoleBinding{
+			TypeMeta: k8s_meta.TypeMeta{},
+			ObjectMeta: k8s_meta.ObjectMeta{
+				Name:      "sample-role-binding",
+				Namespace: "default",
+				Labels:    map[string]string{"k": "v"},
+			},
+			Subjects: []rbac_authorization_k8s_io_v1.Subject{{
+				Kind:      "ServiceAccount",
+				Name:      "sample-target",
+				Namespace: "default",
+			}},
+			RoleRef: rbac_authorization_k8s_io_v1.RoleRef{
+				APIGroup: "rbac.authorization.k8s.io",
+				Kind:     "Role",
+				Name:     "sample-controller",
 			},
 		}
 		equal := ObjectsEqual(obj1, obj2)
