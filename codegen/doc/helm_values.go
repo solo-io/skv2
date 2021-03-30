49fe560d436ddf2546f9b3305c90c92273fb87c2
Harvey Xia <harvey@harveyxia.com>
2021-03-30 11:55:00 -0400

    Generate helm values doc (#224)
    * add generation for helm values
    * Merge branch 'master' of github.com:solo-io/skv2 into generate-helm-values-doc
    * make map key/val type explicit
    * represent ports as name:number pairs
    * change cl
    * Merge branch 'master' into generate-helm-values-doc
    * Adding changelog file to new location
    * Deleting changelog file from old location

---
+++ codegen/doc/helm_values.go
@@ -0,0 +1,210 @@
+package doc
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"sort"
+	"strings"
+	"unicode"
+
+	"github.com/solo-io/go-utils/contextutils"
+)
+
+const (
+
+	// if specified, ignore all descendants
+	omitChildrenTag = "omitChildren"
+
+	// the field name
+	jsonTag = "json"
+
+	// the field description
+	descTag = "desc"
+
+	// markdown file header
+	header = `
+---
+title: "%s"
+description: Reference for Helm values.
+weight: 2
+---
+`
+)
+
+type HelmValue struct {
+	Key          string
+	Type         string
+	DefaultValue string
+	Description  string
+}
+
+type HelmValues []HelmValue
+
+func (v HelmValues) ToMarkdown(title string) string {
+	result := new(strings.Builder)
+	fmt.Fprintln(result, fmt.Sprintf(header, title))
+	fmt.Fprintln(result, "|Option|Type|Default Value|Description|")
+	fmt.Fprintln(result, "|------|----|-----------|-------------|")
+	for _, value := range v {
+		fmt.Fprintf(result, "|%s|%s|%s|%s|\n", value.Key, value.Type, value.DefaultValue, value.Description)
+	}
+	return result.String()
+}
+
+type addValue func(HelmValue)
+
+func GenerateHelmValuesDoc(s interface{}, topLevelKey string, topLevelDesc string) HelmValues {
+	var values []HelmValue
+	cfgT := reflect.ValueOf(s)
+	addValue := func(v HelmValue) { values = append(values, v) }
+
+	var path []string
+	if topLevelKey != "" {
+		path = []string{topLevelKey}
+	}
+
+	docReflect(addValue, path, topLevelDesc, cfgT.Type(), cfgT)
+
+	return values
+}
+
+func docReflect(addValue addValue, path []string, desc string, typ reflect.Type, val reflect.Value) {
+	switch typ.Kind() {
+	case reflect.Ptr:
+		var elemVal reflect.Value
+		if elemVal != val {
+			elemVal = val.Elem()
+		}
+		docReflect(addValue, path, desc, typ.Elem(), elemVal)
+	case reflect.Map:
+
+		// add entry for map itself
+		addValue(HelmValue{Key: strings.Join(path, "."), Type: getMapType(typ), DefaultValue: valToString(val), Description: desc})
+
+		if typ.Key().Kind() == reflect.String {
+			docReflect(addValue, append(path, "<MAP_KEY>"), desc, typ.Elem(), reflect.Value{})
+
+			if (val != reflect.Value{}) {
+				// sort map keys for deterministic generation
+				sortedKeys := val.MapKeys()
+				sort.Slice(sortedKeys, func(i, j int) bool {
+					return sortedKeys[i].String() < sortedKeys[j].String()
+				})
+
+				for _, k := range sortedKeys {
+					elemPath := append(path, k.String())
+					defaultVal := val.MapIndex(k)
+					if typ.Elem().Kind() <= reflect.Float64 || typ.Elem().Kind() == reflect.String {
+						// primitive type, print it as default value
+						valStr := valToString(defaultVal)
+						addValue(HelmValue{Key: strings.Join(elemPath, "."), Type: typ.Elem().Kind().String(), DefaultValue: valStr, Description: desc})
+					} else {
+						// non primitive type, descend
+						docReflect(addValue, elemPath, desc, typ.Elem(), defaultVal)
+					}
+				}
+			}
+		}
+	case reflect.Slice:
+		lst := len(path) - 1
+		path[lst] = path[lst] + "[]"
+
+		// add entry for slice field itself
+		addValue(HelmValue{Key: strings.Join(path, "."), Type: "[]" + typ.Elem().Kind().String(), DefaultValue: valToString(val), Description: desc})
+
+		docReflect(addValue, path, desc, typ.Elem(), reflect.Value{})
+	case reflect.Struct:
+
+		// add entry for struct field itself, ignoring the top level struct
+		if len(path) > 0 {
+			addValue(HelmValue{Key: strings.Join(path, "."), Type: typ.Kind().String(), DefaultValue: valToString(val), Description: desc})
+		}
+
+		for i := 0; i < typ.NumField(); i++ {
+			field := typ.Field(i)
+
+			// ignore any private fields
+			// specifically, this avoids an issue where internal proto struct fields cause infinite recursion
+			if len(field.Name) > 0 && unicode.IsLower(rune(field.Name[0])) {
+				continue
+			}
+
+			jsonTag := field.Tag.Get(jsonTag)
+			parts := strings.Split(jsonTag, ",")
+			jsonName := parts[0]
+			desc := field.Tag.Get(descTag)
+			fieldPath := path
+			if jsonName != "" {
+				fieldPath = append(fieldPath, jsonName)
+			}
+			var fieldVal reflect.Value
+			if val != fieldVal {
+				fieldVal = val.Field(i)
+			}
+
+			// ignore the children of fields that are marked as such (i.e. don't recurse down)
+			if _, ok := field.Tag.Lookup(omitChildrenTag); ok {
+				path := strings.Join(append(path, field.Name), ".")
+				kind := field.Type.Kind()
+				if kind == reflect.Slice {
+					path += "[]"
+				} else if kind == reflect.Ptr {
+					// get the underlying type of pointer types
+					kind = fieldVal.Elem().Kind()
+				}
+				// add a HelmValue for the struct field whose children are ignored
+				addValue(HelmValue{Key: path, Type: kind.String(), DefaultValue: valToString(fieldVal), Description: desc})
+				continue
+			}
+
+			docReflect(addValue, fieldPath, desc, field.Type, fieldVal)
+		}
+	default:
+		addValue(HelmValue{Key: strings.Join(path, "."), Type: typ.Kind().String(), DefaultValue: valToString(val), Description: desc})
+	}
+}
+
+func valToString(val reflect.Value) string {
+	var valStr string
+	if val.IsValid() {
+		switch val.Kind() {
+		case reflect.Bool:
+			valStr = fmt.Sprint(val.Bool())
+		case reflect.String:
+			valStr = fmt.Sprint(val.String())
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			valStr = fmt.Sprint(val.Int())
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			valStr = fmt.Sprint(val.Uint())
+		case reflect.Ptr:
+			return valToString(val.Elem())
+		case reflect.Slice, reflect.Struct:
+			j, err := json.Marshal(val.Interface())
+			if err != nil {
+				contextutils.LoggerFrom(context.Background()).Warnf("failed to marshal value to json: %v", err)
+			}
+			valStr = string(j)
+		}
+	}
+	// needed for correct markdown table formatting (can't have "||")
+	if len(valStr) == 0 {
+		valStr = " "
+	}
+	return valStr
+}
+
+// get string representation of map type in the form "map[keyType, valType]"
+func getMapType(val reflect.Type) string {
+	elemType := val.Elem().Kind().String()
+	if val.Elem().Kind() == reflect.Ptr {
+		elemType = val.Elem().Elem().Kind().String()
+	}
+
+	return fmt.Sprintf(
+		"map[%s, %s]",
+		val.Key().Kind().String(),
+		elemType,
+	)
+}
--- codegen/model/chart.go
+++ codegen/model/chart.go
@@ -1,6 +1,10 @@
 package model
 
 import (
+	"fmt"
+
+	"github.com/iancoleman/strcase"
+	"github.com/solo-io/skv2/codegen/doc"
 	v1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 )
@@ -59,18 +63,18 @@ type Service struct {
 }
 type ServicePort struct {
 	// The name of this port within the service.
-	Name string `json:"name,omitempty" protobuf:"bytes,1,opt,name=name"`
+	Name string `json:"name,omitempty" protobuf:"bytes,1,opt,name=name" desc:"The name of this port within the service."`
 
 	// The default port that will be exposed by this service.
-	DefaultPort int32 `json:"port" protobuf:"varint,3,opt,name=port"`
+	DefaultPort int32 `json:"port" protobuf:"varint,3,opt,name=port" desc:"The default port that will be exposed by this service."`
 }
 
 type Image struct {
-	Tag        string        `json:"tag,omitempty"  desc:"tag for the container"`
-	Repository string        `json:"repository,omitempty"  desc:"image name (repository) for the container."`
-	Registry   string        `json:"registry,omitempty" desc:"image prefix/registry e.g. (quay.io/solo-io)"`
-	PullPolicy v1.PullPolicy `json:"pullPolicy,omitempty"  desc:"image pull policy for the container"`
-	PullSecret string        `json:"pullSecret,omitempty" desc:"image pull policy for the container "`
+	Tag        string        `json:"tag,omitempty"  desc:"Tag for the container."`
+	Repository string        `json:"repository,omitempty"  desc:"Image name (repository)."`
+	Registry   string        `json:"registry,omitempty" desc:"Image registry."`
+	PullPolicy v1.PullPolicy `json:"pullPolicy,omitempty"  desc:"Image pull policy."`
+	PullSecret string        `json:"pullSecret,omitempty" desc:"Image pull policy. "`
 }
 
 // Helm chart dependency
@@ -90,4 +94,60 @@ type Data struct {
 	Icon         string       `json:"icon,omitempty"`
 	Sources      []string     `json:"sources,omitempty"`
 	Dependencies []Dependency `json:"dependencies,omitempty"`
+}
+
+// fields exposed as Helm values
+type HelmValues struct {
+	Operators    map[string]OperatorHelmValues
+	CustomValues interface{}
+}
+
+type OperatorHelmValues struct {
+	Image        Image                    `json:"image" desc:"Specify the deployment image."`
+	Resources    *v1.ResourceRequirements `json:"resources" desc:"Specify deployment resource requirements. See the [Kubernetes documentation](https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.20/#resourcerequirements-v1-core) for specification details." omitChildren:"true"`
+	ServiceType  v1.ServiceType           `json:"serviceType" desc:"Specify the service type. Can be either \"ClusterIP\", \"NodePort\", \"LoadBalancer\", or \"ExternalName\"."`
+	ServicePorts map[string]uint32        `json:"ports" desc:"Specify service ports as a map from port name to port number."`
+	Env          []v1.EnvVar              `json:"env" desc:"Specify environment variables for the deployment. See the [Kubernetes documentation](https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.20/#envvarsource-v1-core) for specification details." omitChildren:"true"`
+}
+
+func (c Chart) BuildChartValues() HelmValues {
+	values := HelmValues{}
+	values.CustomValues = c.Values
+	values.Operators = map[string]OperatorHelmValues{}
+
+	for _, operator := range c.Operators {
+		servicePorts := map[string]uint32{}
+		for _, port := range operator.Service.Ports {
+			servicePorts[port.Name] = uint32(port.DefaultPort)
+		}
+
+		values.Operators[operator.Name] = OperatorHelmValues{
+			Image:        operator.Deployment.Image,
+			Resources:    operator.Deployment.Resources,
+			ServiceType:  operator.Service.Type,
+			ServicePorts: servicePorts,
+			Env:          operator.Env,
+		}
+	}
+
+	return values
+}
+
+func (c Chart) GenerateHelmDoc(title string) string {
+	helmValues := c.BuildChartValues()
+
+	// generate documentation for custom values
+	helmValuesForDoc := doc.GenerateHelmValuesDoc(helmValues.CustomValues, "", "")
+
+	// generate documentation for operator values
+	for name, values := range helmValues.Operators {
+		name = strcase.ToLowerCamel(name)
+
+		// clear image tag so it doesn't show build time commit hashes
+		values.Image.Tag = ""
+
+		helmValuesForDoc = append(helmValuesForDoc, doc.GenerateHelmValuesDoc(values, name, fmt.Sprintf("Configuration for the %s deployment.", name))...)
+	}
+
+	return helmValuesForDoc.ToMarkdown(title)
 }
