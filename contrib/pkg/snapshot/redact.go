95cbfb2e57ff33687ee4f5c42d60be1c523c4d85
Eitan Yarmush <eitan.yarmush@solo.io>
2021-08-30 14:15:01 -0400

    Fix input marshal (#288)
    * fix redact to cover last applied config
    
    * working
    
    * added changelog

--- contrib/pkg/snapshot/redact.go
+++ contrib/pkg/snapshot/redact.go
@@ -5,12 +5,24 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+const (
+	redactedString = "<redacted>"
+)
+
 // RedactSecretData returns a copy with sensitive information redacted
 func RedactSecretData(obj client.Object) client.Object {
 	if sec, ok := obj.(*v1.Secret); ok {
 		redacted := sec.DeepCopyObject().(*v1.Secret)
 		for k := range redacted.Data {
-			redacted.Data[k] = []byte("*****")
+			redacted.Data[k] = []byte(redactedString)
+		}
+
+		// Also need to check for kubectl apply, last applied config.
+		// Secret data can be found there as well if that's how the secret is created
+		for key, _ := range redacted.Annotations {
+			if key == v1.LastAppliedConfigAnnotation {
+				redacted.Annotations[key] = redactedString
+			}
 		}
 		return redacted
 	}
