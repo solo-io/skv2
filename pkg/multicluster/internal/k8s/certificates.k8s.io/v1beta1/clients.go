390150f29d68b200867c427269196145fdea346e
Jahvon Dockery <jahvon.dockery@solo.io>
2021-09-08 16:28:44 -0400

    codegen

--- pkg/multicluster/internal/k8s/certificates.k8s.io/v1beta1/clients.go
+++ pkg/multicluster/internal/k8s/certificates.k8s.io/v1beta1/clients.go
@@ -50,7 +50,7 @@ type clientSet struct {
 
 func NewClientsetFromConfig(cfg *rest.Config) (Clientset, error) {
 	scheme := scheme.Scheme
-	if err := certificates_k8s_io_v1beta1.AddToScheme(scheme); err != nil {
+	if err := certificates_k8s_io_v1beta1.SchemeBuilder.AddToScheme(scheme); err != nil {
 		return nil, err
 	}
 	client, err := client.New(cfg, client.Options{
