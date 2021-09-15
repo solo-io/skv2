af68823d7b1613271a1366df79c676a737894dfb
Eitan Yarmush <eitan.yarmush@solo.io>
2021-09-15 12:46:04 -0400

    remove service entry stuff

--- pkg/controllerutils/equality.go
+++ pkg/controllerutils/equality.go
@@ -4,12 +4,9 @@ import (
 	"reflect"
 
 	"github.com/solo-io/skv2/pkg/equalityutils"
-	"istio.io/api/networking/v1alpha3"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
-
-type EqualityFunc = func(val1, val2 interface{}) bool
 
 // returns true if "relevant" parts of obj1 and obj2 have equal:
 // - labels,
@@ -18,7 +15,7 @@ type EqualityFunc = func(val1, val2 interface{}) bool
 // - non-metadata, non-status fields
 // Note that Status fields are not compared.
 // To compare status fields, use ObjectStatusesEqual
-func ObjectsEqual(obj1, obj2 runtime.Object, equalityFunc ...EqualityFunc) bool {
+func ObjectsEqual(obj1, obj2 runtime.Object) bool {
 	value1, value2 := reflect.ValueOf(obj1), reflect.ValueOf(obj2)
 
 	if value1.Type() != value2.Type() {
@@ -55,163 +52,12 @@ func ObjectsEqual(obj1, obj2 runtime.Object, equalityFunc ...EqualityFunc) bool 
 		field1 := mkPointer(value1.Field(i))
 		field2 := mkPointer(value2.Field(i))
 
-		// for _, v := range
-
-		field1ProtoVal, field1IsProto := field1.(*v1alpha3.ServiceEntry)
-		if field1IsProto {
-			field2ProtoVal, field2IsProto := field2.(*v1alpha3.ServiceEntry)
-			if field2IsProto {
-				if !serviceEntryEqual(field1ProtoVal, field2ProtoVal) {
-					return false
-				}
-				continue
-			}
-		}
-
 		// assert DeepEquality any other fields
 		if !equalityutils.DeepEqual(field1, field2) {
 			return false
 		}
 	}
 
-	return true
-}
-//
-// func destinationRuleEqual(val1, val2 *v1alpha3.DestinationRule) bool {
-// 	if val1 == nil && val2 == nil {
-// 		return true
-// 	}
-//
-// 	if val1.GetHost() != val2.GetHost() {
-// 		return false
-// 	}
-//
-// 	if len(val1.GetExportTo()) != len(val2.GetExportTo()) {
-// 		return false
-// 	} else {
-// 		for idx, val := range val1.GetExportTo() {
-// 				if val != val2.GetExportTo()[idx] {
-// 					return false
-// 				}
-// 		}
-// 	}
-//
-// 	if len(val1.GetSubsets()) != len(val2.GetSubsets()) {
-// 		return false
-// 	} else {
-//
-// 	}
-//
-// 	if
-//
-// }
-
-func serviceEntryEqual(val1, val2 *v1alpha3.ServiceEntry) bool {
-	if val1 == nil && val2 == nil {
-		return true
-	}
-
-	if len(val1.GetHosts()) != len(val2.GetHosts()) {
-		return false
-	} else {
-		for idx, val := range val1.GetHosts() {
-			if val != val2.GetHosts()[idx] {
-				return false
-			}
-		}
-	}
-
-	if len(val1.GetAddresses()) != len(val2.GetAddresses()) {
-		return false
-	} else {
-		for idx, val := range val1.GetAddresses() {
-			if val != val2.GetAddresses()[idx] {
-				return false
-			}
-		}
-	}
-
-	if len(val1.GetExportTo()) != len(val2.GetExportTo()) {
-		return false
-	} else {
-		for idx, val := range val1.GetExportTo() {
-			if val != val2.GetExportTo()[idx] {
-				return false
-			}
-		}
-	}
-
-	if len(val1.GetSubjectAltNames()) != len(val2.GetSubjectAltNames()) {
-		return false
-	} else {
-		for idx, val := range val1.GetSubjectAltNames() {
-			if val != val2.GetSubjectAltNames()[idx] {
-				return false
-			}
-		}
-	}
-
-	if len(val1.GetEndpoints()) != len(val2.GetEndpoints()) {
-		return false
-	} else {
-		for idx, ep := range val1.GetEndpoints() {
-			ep2 := val2.GetEndpoints()[idx]
-			if !mapStringEqual(ep.GetLabels(), ep2.GetLabels()) {
-				return false
-			}
-
-			if !reflect.DeepEqual(ep.GetPorts(), ep2.GetPorts()) {
-				return false
-			}
-			if ep.GetAddress() != ep2.GetAddress() {
-				return false
-			}
-			if ep.GetLocality() != ep2.GetLocality() {
-				return false
-			}
-			if ep.GetServiceAccount() != ep2.GetServiceAccount() {
-				return false
-			}
-			if ep.GetWeight() != ep2.GetWeight() {
-				return false
-			}
-			if ep.GetNetwork() != ep2.GetNetwork() {
-				return false
-			}
-		}
-	}
-
-	if len(val1.GetPorts()) != len(val2.GetPorts()) {
-		return false
-	} else {
-		for idx, port := range val1.GetPorts() {
-			port2 := val2.GetPorts()[idx]
-			if port.GetName() != port2.GetName() {
-				return false
-			}
-			if port.GetNumber() != port2.GetNumber() {
-				return false
-			}
-			if port.GetTargetPort() != port2.GetTargetPort() {
-				return false
-			}
-			if port.GetProtocol() != port2.GetProtocol() {
-				return false
-			}
-		}
-	}
-
-	if !reflect.DeepEqual(val1.GetWorkloadSelector().GetLabels(), val2.GetWorkloadSelector().GetLabels()) {
-		return false
-	}
-
-	if val1.GetResolution() != val2.GetResolution() {
-		return false
-	}
-
-	if val1.GetLocation() != val2.GetLocation() {
-		return false
-	}
 	return true
 }
 
