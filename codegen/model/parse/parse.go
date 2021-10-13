f2bb113806ded1155217b3d0c617712274ade524
Jahvon Dockery <jahvon.dockery@solo.io>
2021-10-13 15:28:50 -0400

    Skip crds when specified

--- codegen/model/parse/parse.go
+++ codegen/model/parse/parse.go
@@ -15,7 +15,7 @@ import (
  * This is used by downstream projects to collect the spec hashes of all CRDs, and the version of the product.
  * the resulting CRDMetadata will most likely end up on the downstream project's deployment annotations.
  */
-func ParseCRDs(f fs.FS) (crdutils.CRDMetadata, error) {
+func ParseCRDs(f fs.FS, ignoredFiles map[string]struct{}) (crdutils.CRDMetadata, error) {
 	var ret crdutils.CRDMetadata
 	// read all crds from the specified directory
 	// generate a file with all the versions and hashes of the crds
@@ -27,6 +27,11 @@ func ParseCRDs(f fs.FS) (crdutils.CRDMetadata, error) {
 	var errs error
 
 	for _, dir := range dirList {
+		// Skip files that should be ignored while parsing directory
+		if _, ok := ignoredFiles[dir.Name()]; !ok {
+			continue
+		}
+
 		if !strings.HasSuffix(dir.Name(), ".yaml") {
 			continue
 		}
