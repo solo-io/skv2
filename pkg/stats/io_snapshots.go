9f643ca5fb94852c5e42a5c7e544c8a9c32344a4
Eitan Yarmush <eitan.yarmush@solo.io>
2022-01-20 20:33:20 -0500

    combined snapshots

--- pkg/stats/io_snapshots.go
+++ pkg/stats/io_snapshots.go
@@ -7,66 +7,105 @@ import (
 	"sync"
 )
 
-type SnapshotHistory struct {
+type SnapshotHistory interface {
+	// SetInput Sets the input snapshot for the given component
+	SetInput(id string, latestInput json.Marshaler)
+	// GetInput gets the input snapshot for all components
+	GetInput() ([]byte, error)
+	// SetOutput Sets the output snapshot for the given component
+	SetOutput(id string, latestOutput json.Marshaler)
+	// GetOutput gets the output snapshot for all component
+	GetOutput() ([]byte, error)
+}
+
+type snapshotHistory struct {
 	lock         sync.RWMutex
-	latestInput  json.Marshaler
-	latestOutput json.Marshaler
+	latestInput  map[string]json.Marshaler
+	latestOutput map[string]json.Marshaler
 }
 
-func NewSnapshotHistory() *SnapshotHistory {
-	return &SnapshotHistory{}
+func NewSnapshotHistory() SnapshotHistory {
+	return &snapshotHistory{}
 }
 
-func (h *SnapshotHistory) SetInput(latestInput json.Marshaler) {
+func (h *snapshotHistory) SetInput(id string, latestInput json.Marshaler) {
 	h.lock.Lock()
 	defer h.lock.Unlock()
-	h.latestInput = latestInput
+	h.latestInput[id] = latestInput
 }
 
-func (h *SnapshotHistory) GetInput() ([]byte, error) {
+func (h *snapshotHistory) GetInput() ([]byte, error) {
 	h.lock.RLock()
 	defer h.lock.RUnlock()
 	if h.latestInput == nil {
 		return []byte{}, nil
 	}
-	return h.latestInput.MarshalJSON()
+	result := map[string]interface{}{}
+	for id, obj := range h.latestInput {
+		jsn, err := obj.MarshalJSON()
+		if err != nil {
+			return nil, err
+		}
+		future := map[string]interface{}{}
+		if err := json.Unmarshal(jsn, &future); err != nil {
+			return nil, err
+		}
+		result[id] = future
+	}
+	return json.Marshal(result)
 }
 
-func (h *SnapshotHistory) SetOutput(latestOutput json.Marshaler) {
+func (h *snapshotHistory) SetOutput(id string, latestOutput json.Marshaler) {
 	h.lock.Lock()
 	defer h.lock.Unlock()
-	h.latestOutput = latestOutput
+	h.latestOutput[id] = latestOutput
 }
 
-func (h *SnapshotHistory) GetOutput() ([]byte, error) {
+func (h *snapshotHistory) GetOutput() ([]byte, error) {
 	h.lock.RLock()
 	defer h.lock.RUnlock()
 	if h.latestOutput == nil {
 		return []byte{}, nil
 	}
-	return h.latestOutput.MarshalJSON()
+	result := map[string]interface{}{}
+	for id, obj := range h.latestOutput {
+		jsn, err := obj.MarshalJSON()
+		if err != nil {
+			return nil, err
+		}
+		future := map[string]interface{}{}
+		if err := json.Unmarshal(jsn, &future); err != nil {
+			return nil, err
+		}
+		result[id] = future
+	}
+	return json.Marshal(result)
 }
 
-func AddSnapshots(mux *http.ServeMux, history *SnapshotHistory) {
+func AddSnapshots(mux *http.ServeMux, history SnapshotHistory) {
 
 	profileDescriptions["/snapshots/input"] = "Latest Input Snapshot"
 	profileDescriptions["/snapshots/output"] = "Latest Output Snapshot"
 
-	mux.HandleFunc("/snapshots/input", func(w http.ResponseWriter, r *http.Request) {
-		b, err := history.GetInput()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		_, _ = fmt.Fprint(w, string(b))
-	})
+	mux.HandleFunc(
+		"/snapshots/input", func(w http.ResponseWriter, r *http.Request) {
+			b, err := history.GetInput()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			_, _ = fmt.Fprint(w, string(b))
+		},
+	)
 
-	mux.HandleFunc("/snapshots/output", func(w http.ResponseWriter, r *http.Request) {
-		b, err := history.GetOutput()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		_, _ = fmt.Fprint(w, string(b))
-	})
+	mux.HandleFunc(
+		"/snapshots/output", func(w http.ResponseWriter, r *http.Request) {
+			b, err := history.GetOutput()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			_, _ = fmt.Fprint(w, string(b))
+		},
+	)
 }
