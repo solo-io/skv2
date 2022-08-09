eda7a0c17ccadea774b80e871f17f889e5f8d508
Eitan Yarmush <eitan.yarmush@solo.io>
2022-08-09 12:57:42 -0400

    generic client.Client

---
+++ pkg/client/client.go
@@ -0,0 +1,152 @@
+package client
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type Reader[T client.Object, L client.ObjectList] interface {
+
+	// Get retrieves an obj for the given object key from the Kubernetes Cluster.
+	// obj must be a struct pointer so that obj can be updated with the response
+	// returned by the Server.
+	Get(ctx context.Context, key client.ObjectKey) (T, error)
+
+	// List retrieves list of objects for a given namespace and list options. On a
+	// successful call, Items field in the list will be populated with the
+	// result returned from the server.
+	List(ctx context.Context, opts ...client.ListOption) (L, error)
+}
+
+type Writer[T client.Object] interface {
+	// Create saves the object obj in the Kubernetes cluster.
+	Create(ctx context.Context, obj T, opts ...client.CreateOption) error
+
+	// Delete deletes the given obj from Kubernetes cluster.
+	Delete(ctx context.Context, obj T, opts ...client.DeleteOption) error
+
+	// Update updates the given obj in the Kubernetes cluster. obj must be a
+	// struct pointer so that obj can be updated with the content returned by the Server.
+	Update(ctx context.Context, obj T, opts ...client.UpdateOption) error
+
+	// Patch patches the given obj in the Kubernetes cluster. obj must be a
+	// struct pointer so that obj can be updated with the content returned by the Server.
+	Patch(ctx context.Context, obj T, patch client.Patch, opts ...client.PatchOption) error
+
+	// DeleteAllOf deletes all objects of the given type matching the given options.
+	DeleteAllOf(ctx context.Context, obj T, opts ...client.DeleteAllOfOption) error
+}
+
+// StatusClient knows how to create a client which can update status subresource
+// for kubernetes objects.
+type StatusClient[T client.Object] interface {
+	Status() StatusWriter[T]
+}
+
+// StatusWriter knows how to update status subresource of a Kubernetes object.
+type StatusWriter[T client.Object] interface {
+	// Update updates the fields corresponding to the status subresource for the
+	// given obj. obj must be a struct pointer so that obj can be updated
+	// with the content returned by the Server.
+	Update(ctx context.Context, obj T, opts ...client.UpdateOption) error
+
+	// Patch patches the given object's subresource. obj must be a struct
+	// pointer so that obj can be updated with the content returned by the
+	// Server.
+	Patch(ctx context.Context, obj T, patch client.Patch, opts ...client.PatchOption) error
+}
+
+type GenericClient[T client.Object,  L client.ObjectList] interface {
+	Reader[T, L]
+	Writer[T]
+	StatusClient[T]
+
+	// Scheme returns the scheme this client is using.
+	Scheme() *runtime.Scheme
+	// RESTMapper returns the rest this client is using.
+	RESTMapper() meta.RESTMapper
+}
+
+func NewClientset[T client.Object,  L client.ObjectList](cli client.Client) GenericClient[T, L] {
+	return &genericClient[T, L]{
+		genericClient: cli,
+	}
+}
+
+func NewClientsetFromConfig[T client.Object,  L client.ObjectList](cfg *rest.Config) (GenericClient[T, L], error) {
+	cli, err := client.New(cfg, client.Options{})
+	if err != nil {
+		return nil, err
+	}
+	return NewClientset[T, L](cli), nil
+}
+
+type genericClient[T client.Object,  L client.ObjectList] struct {
+	genericClient client.Client
+}
+
+func (g *genericClient[T, L]) Get(ctx context.Context, key client.ObjectKey) (T, error) {
+	obj := new(T)
+	if err := g.genericClient.Get(ctx, key, *obj); err != nil {
+		return nil, err
+	}
+	return *obj, nil
+}
+
+func (g *genericClient[T, L]) List(ctx context.Context, opts ...client.ListOption) (L, error) {
+	list := new(L)
+	if err := g.genericClient.List(ctx, *list, opts...); err != nil {
+		return nil, err
+	}
+	return *list, nil
+}
+
+func (g *genericClient[T, L]) Create(ctx context.Context, obj T, opts ...client.CreateOption) error {
+	return g.genericClient.Create(ctx, obj, opts...)
+}
+
+func (g *genericClient[T, L]) Delete(ctx context.Context, obj T, opts ...client.DeleteOption) error {
+	return g.genericClient.Delete(ctx, obj, opts...)
+}
+
+func (g *genericClient[T, L]) Update(ctx context.Context, obj T, opts ...client.UpdateOption) error {
+	return g.genericClient.Update(ctx, obj, opts...)
+}
+
+func (g *genericClient[T, L]) Patch(ctx context.Context, obj T, patch client.Patch, opts ...client.PatchOption) error {
+	return g.genericClient.Patch(ctx, obj, patch, opts...)
+}
+
+func (g *genericClient[T, L]) DeleteAllOf(ctx context.Context, obj T, opts ...client.DeleteAllOfOption) error {
+	return g.genericClient.DeleteAllOf(ctx, obj, opts...)
+}
+
+func (g *genericClient[T, L]) Status() StatusWriter[T] {
+	return &statusWriter[T]{
+		statusWriter: g.genericClient.Status(),
+	}
+}
+
+func (g *genericClient[T, L]) Scheme() *runtime.Scheme {
+	return g.genericClient.Scheme()
+}
+
+func (g *genericClient[T, L]) RESTMapper() meta.RESTMapper {
+	return g.genericClient.RESTMapper()
+}
+
+type statusWriter[T client.Object] struct {
+	statusWriter client.StatusWriter
+}
+
+func (s *statusWriter[T]) Update(ctx context.Context, obj T, opts ...client.UpdateOption) error {
+	return s.statusWriter.Update(ctx, obj, opts...)
+}
+
+func (s *statusWriter[T]) Patch(ctx context.Context, obj T, patch client.Patch, opts ...client.PatchOption) error {
+	return s.statusWriter.Patch(ctx, obj, patch, opts...)
+}
