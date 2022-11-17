4300cddb176a8531545bb2fd924c4cb68c2ac04b
Conrad Hanson <27842017+conradhanson@users.noreply.github.com>
2022-11-17 14:09:08 -0800

    add function to allow ClusterObjectRef struct to satisfy the ClusterResourceId interface

--- pkg/ezkube/resource_id.go
+++ pkg/ezkube/resource_id.go
@@ -1,6 +1,8 @@
 package ezkube
 
-import "sigs.k8s.io/controller-runtime/pkg/client"
+import (
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
 
 const ClusterAnnotation = "cluster.solo.io/cluster"
 
@@ -15,6 +17,39 @@ type ClusterResourceId interface {
 	GetName() string
 	GetNamespace() string
 	GetAnnotations() map[string]string
+}
+
+// internal struct needed to create helper func that converts ref to struct that satisfies ClusterResourceId interface
+type clusterResourceId struct {
+	Name, Namespace string
+	Annotations     map[string]string
+}
+
+func (c clusterResourceId) GetName() string {
+	return c.Name
+}
+
+func (c clusterResourceId) GetNamespace() string {
+	return c.Namespace
+}
+
+func (c clusterResourceId) GetAnnotations() map[string]string {
+	return c.Annotations
+}
+
+type deprecatedClusterResourceId interface {
+	GetName() string
+	GetNamespace() string
+	GetClusterName() string
+}
+
+// ConvertRefToId converts a ClusterObjectRef to a struct that implements the ClusterResourceId interface
+func ConvertRefToId(ref deprecatedClusterResourceId) ClusterResourceId {
+	return clusterResourceId{
+		Name:        ref.GetName(),
+		Namespace:   ref.GetNamespace(),
+		Annotations: map[string]string{ClusterAnnotation: ref.GetClusterName()},
+	}
 }
 
 func GetClusterName(id ClusterResourceId) string {
