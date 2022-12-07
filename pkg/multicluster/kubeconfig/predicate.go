cdb649081f608fbdf040f55aed5afeec86acaae4
Jenny Shu <jenny.shu@solo.io>
2022-12-07 14:37:56 -0500

    list

--- pkg/multicluster/kubeconfig/predicate.go
+++ pkg/multicluster/kubeconfig/predicate.go
@@ -3,36 +3,64 @@ package kubeconfig
 import (
 	"reflect"
 
-	"sigs.k8s.io/controller-runtime/pkg/client"
-
 	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 )
 
-var Predicate = predicate.Funcs{
-	CreateFunc: func(e event.CreateEvent) bool {
-		return isKubeConfigSecret(e.Object)
-	},
-	DeleteFunc: func(e event.DeleteEvent) bool {
-		return isKubeConfigSecret(e.Object)
-	},
-	UpdateFunc: func(e event.UpdateEvent) bool {
-		if isKubeConfigSecret(e.ObjectNew) {
-			// Ignore metadata changes.
-			oldSecret, newSecret := e.ObjectOld.(*corev1.Secret), e.ObjectNew.(*corev1.Secret)
-			return !reflect.DeepEqual(oldSecret.Data, newSecret.Data)
-		}
-		return false
-	},
-	GenericFunc: func(e event.GenericEvent) bool {
-		return isKubeConfigSecret(e.Object)
-	},
+func BuildPredicate(watchNamespaces []string) predicate.Funcs {
+	return predicate.Funcs{
+		CreateFunc: func(e event.CreateEvent) bool {
+			if !isNamespaceWatched(watchNamespaces, e.Object.GetNamespace()) {
+				return false
+			}
+			return isKubeConfigSecret(e.Object)
+		},
+		DeleteFunc: func(e event.DeleteEvent) bool {
+			if !isNamespaceWatched(watchNamespaces, e.Object.GetNamespace()) {
+				return false
+			}
+			return isKubeConfigSecret(e.Object)
+		},
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			if !isNamespaceWatched(watchNamespaces, e.ObjectNew.GetNamespace()) {
+				return false
+			}
+			if isKubeConfigSecret(e.ObjectNew) {
+				// Ignore metadata changes.
+				oldSecret, newSecret := e.ObjectOld.(*corev1.Secret), e.ObjectNew.(*corev1.Secret)
+				return !reflect.DeepEqual(oldSecret.Data, newSecret.Data)
+			}
+			return false
+		},
+		GenericFunc: func(e event.GenericEvent) bool {
+			if !isNamespaceWatched(watchNamespaces, e.Object.GetNamespace()) {
+				return false
+			}
+			return isKubeConfigSecret(e.Object)
+		},
+	}
 }
 
 func isKubeConfigSecret(obj client.Object) bool {
 	if s, ok := obj.(*corev1.Secret); ok {
 		return s.Type == SecretType
+	}
+	return false
+}
+
+// If watchNamespaces is empty, then watch all namespaces. Otherwise, watch
+// only the specified namespaces.
+func isNamespaceWatched(watchNamespaces []string, namespace string) bool {
+	if len(watchNamespaces) == 0 {
+		return true
+	}
+
+	for _, ns := range watchNamespaces {
+		if ns == namespace {
+			return true
+		}
 	}
 	return false
 }
--- pkg/multicluster/watch/watcher.go
+++ pkg/multicluster/watch/watcher.go
@@ -20,37 +20,39 @@ import (
 )
 
 type clusterWatcher struct {
-	ctx       context.Context
-	handlers  *handlerList
-	managers  *managerSet
-	options   manager.Options
-	namespace string
+	ctx             context.Context
+	handlers        *handlerList
+	managers        *managerSet
+	options         manager.Options
+	watchNamespaces []string
 }
 
 var _ multicluster.Interface = &clusterWatcher{}
 
-// NewClusterWatcher returns a *clusterWatcher.
+// NewClusterWatcher returns a *clusterWatcher, which watches for changes to kubeconfig secrets
+// (which contain kubeconfigs for remote clusters).
 // When ctx is cancelled, all cluster managers started by the clusterWatcher are stopped.
 // Provided manager.Options are applied to all managers started by the clusterWatcher.
-func NewClusterWatcher(ctx context.Context, options manager.Options, namespace string) *clusterWatcher {
+// If watchNamespaces is not empty, only secrets in the given namespaces will be watched. If empty, secrets in
+// all namespaces will be watched.
+func NewClusterWatcher(ctx context.Context, options manager.Options, watchNamespaces []string) *clusterWatcher {
 	return &clusterWatcher{
-		ctx:       ctx,
-		handlers:  newHandlerList(),
-		managers:  newManagerSet(),
-		options:   options,
-		namespace: namespace,
+		ctx:             ctx,
+		handlers:        newHandlerList(),
+		managers:        newManagerSet(),
+		options:         options,
+		watchNamespaces: watchNamespaces,
 	}
 }
 
 func (c *clusterWatcher) Run(master manager.Manager) error {
 	loop := controller.NewSecretReconcileLoop("cluster watcher", master, reconcile.Options{})
-	return loop.RunSecretReconciler(c.ctx, c, kubeconfig.Predicate)
+	return loop.RunSecretReconciler(c.ctx, c, kubeconfig.BuildPredicate(c.watchNamespaces))
 }
 
 func (c *clusterWatcher) ReconcileSecret(obj *v1.Secret) (reconcile.Result, error) {
-	if c.namespace != "" && obj.Namespace != c.namespace {
-		return reconcile.Result{}, nil
-	}
+	contextutils.LoggerFrom(c.ctx).Infow("ReconcileSecret", zap.Any("name", obj.Name), zap.Any("namespace", obj.Namespace))
+
 	clusterName, clientCfg, err := kubeconfig.SecretToConfig(obj)
 	if err != nil {
 		return reconcile.Result{}, eris.Wrap(err, "failed to extract kubeconfig from secret")
@@ -66,6 +68,7 @@ func (c *clusterWatcher) ReconcileSecret(obj *v1.Secret) (reconcile.Result, erro
 		c.removeCluster(clusterName)
 	}
 
+	contextutils.LoggerFrom(c.ctx).Infow("ReconcileSecret - starting manager")
 	c.startManager(clusterName, restCfg)
 
 	return reconcile.Result{}, nil
