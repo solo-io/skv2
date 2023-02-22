7d2dec2c467d8b3857581e88176e5f5c750d0a26
Rachael Graham <rachael.graham@solo.io>
2023-02-22 14:14:10 -0600

    Update helm_chart_values.go

--- codegen/model/values/helm_chart_values.go
+++ codegen/model/values/helm_chart_values.go
@@ -46,32 +46,32 @@ type UserValues struct {
 	UserContainerValues `json:",inline"`
 
 	// Required to have an interface value in order to use the `index` function in the template
-	Sidecars       map[string]UserContainerValues `json:"sidecars" desc:"Configuration for the deployed containers."`
-	FloatingUserID bool                           `json:"floatingUserId" desc:"Allow the pod to be assigned a dynamic user ID."`
+	Sidecars       map[string]UserContainerValues `json:"sidecars" desc:"Optional configuration for the deployed containers."`
+	FloatingUserID bool                           `json:"floatingUserId" desc:"Allow the pod to be assigned a dynamic user ID. Required for OpenShift installations."`
 	RunAsUser      uint32                         `json:"runAsUser" desc:"Static user ID to run the containers as. Unused if floatingUserId is 'true'."`
-	ServiceType    v1.ServiceType                 `json:"serviceType" desc:"Specify the service type. Can be either \"ClusterIP\", \"NodePort\", \"LoadBalancer\", or \"ExternalName\"."`
-	ServicePorts   map[string]uint32              `json:"ports" desc:"Specify service ports as a map from port name to port number."`
+	ServiceType    v1.ServiceType                 `json:"serviceType" desc:"Kubernetes service type. Can be either \"ClusterIP\", \"NodePort\", \"LoadBalancer\", or \"ExternalName\"."`
+	ServicePorts   map[string]uint32              `json:"ports" desc:"Service ports as a map from port name to port number."`
 
 	// Overrides which can be set by the user
-	DeploymentOverrides *appsv1.Deployment `json:"deploymentOverrides,omitempty" desc:"Provide arbitrary overrides for the component's [deployment template](https://kubernetes.io/docs/reference/kubernetes-api/workload-resources/deployment-v1/)" omitChildren:"true"`
-	ServiceOverrides    *v1.Service        `json:"serviceOverrides,omitempty" desc:"Provide arbitrary overrides for the component's [service template](https://kubernetes.io/docs/reference/kubernetes-api/service-resources/service-v1/)." omitChildren:"true"`
+	DeploymentOverrides *appsv1.Deployment `json:"deploymentOverrides,omitempty" desc:"Arbitrary overrides for the component's [deployment template](https://kubernetes.io/docs/reference/kubernetes-api/workload-resources/deployment-v1/)" omitChildren:"true"`
+	ServiceOverrides    *v1.Service        `json:"serviceOverrides,omitempty" desc:"Arbitrary overrides for the component's [service template](https://kubernetes.io/docs/reference/kubernetes-api/service-resources/service-v1/)." omitChildren:"true"`
 
-	Enabled bool `json:"enabled" desc:"Enables or disables creation of the operator deployment/service"`
+	Enabled bool `json:"enabled" desc:"Enable creation of the deployment/service."`
 }
 
 // document values structure for a container
 type UserContainerValues struct {
-	Image           Image                    `json:"image" desc:"Specify the container image"`
-	Env             []v1.EnvVar              `json:"env" desc:"Specify environment variables for the container. See the [Kubernetes documentation](https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.20/#envvarsource-v1-core) for specification details." omitChildren:"true"`
-	Resources       *v1.ResourceRequirements `json:"resources,omitempty" desc:"Specify container resource requirements. See the [Kubernetes documentation](https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.20/#resourcerequirements-v1-core) for specification details."`
-	SecurityContext *v1.SecurityContext      `json:"securityContext,omitempty" desc:"Specify container security context. Set to 'false' to omit the security context entirely. See the [Kubernetes documentation](https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.20/#securitycontext-v1-core) for specification details."`
+	Image           Image                    `json:"image" desc:"Container image."`
+	Env             []v1.EnvVar              `json:"env" desc:"Environment variables for the container. See the [Kubernetes documentation](https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.27/#envvarsource-v1-core) for specification details." omitChildren:"true"`
+	Resources       *v1.ResourceRequirements `json:"resources,omitempty" desc:"Container resource requirements. See the [Kubernetes documentation](https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.27/#resourcerequirements-v1-core) for specification details."`
+	SecurityContext *v1.SecurityContext      `json:"securityContext,omitempty" desc:"Container security context. Set to 'false' to omit the security context entirely. See the [Kubernetes documentation](https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.27/#securitycontext-v1-core) for specification details."`
 }
 
 // this image type is shared with the skv2 Chart model
 type Image struct {
-	Tag        string        `json:"tag,omitempty"  desc:"Tag for the container."`
-	Repository string        `json:"repository,omitempty"  desc:"Image name (repository)."`
+	Tag        string        `json:"tag,omitempty" desc:"Tag for the container."`
+	Repository string        `json:"repository,omitempty" desc:"Image name (repository)."`
 	Registry   string        `json:"registry,omitempty" desc:"Image registry."`
-	PullPolicy v1.PullPolicy `json:"pullPolicy,omitempty"  desc:"Image pull policy."`
+	PullPolicy v1.PullPolicy `json:"pullPolicy,omitempty" desc:"Image pull policy."`
 	PullSecret string        `json:"pullSecret,omitempty" desc:"Image pull secret."`
 }
