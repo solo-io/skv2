1e261f8188ab957e0530a224f6035bf498911d42
Albert <albert.lockett@solo.io>
2023-03-08 09:10:08 -0500

    fix helm docs omitChildren json file name override (#430)
    * fix helm docs omitChildren json file name override
    
    * changelog

--- codegen/doc/helm_values_test.go
+++ codegen/doc/helm_values_test.go
@@ -7,11 +7,39 @@ import (
 )
 
 var _ = Describe("GenerateHelmValuesDoc", func() {
+	It("handles the case where the field has 'omitChildren' and json tag", func() {
+		type ChildType2 struct {
+		}
+		type ChildType struct {
+			Field1 *ChildType2 `json:"myCoolField" desc:"my field" omitChildren:"true"`
+		}
+		result := doc.GenerateHelmValuesDoc(
+			ChildType{},
+			"test",
+			"my test",
+		)
+		expected := doc.HelmValues{
+			{
+				Key:          "test",
+				Type:         "struct",
+				DefaultValue: " ",
+				Description:  "my test",
+			},
+			{
+				Key:          "test.myCoolField",
+				Type:         "struct",
+				DefaultValue: " ",
+				Description:  "my field",
+			},
+		}
+		Expect(result).To(Equal(expected))
+	})
+
 	It("handles case where map value struct has a nil pointer field that omits children", func() {
 		type ChildType2 struct {
 		}
 		type ChildType struct {
-			Field1 *ChildType2 `json:"field1" desc:"my field" omitChildren:"true"`
+			Field1 *ChildType2 `desc:"my field" omitChildren:"true"`
 		}
 
 		type TestType struct {
