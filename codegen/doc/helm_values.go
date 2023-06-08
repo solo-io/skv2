24e0ed6430f00a55583018ca72fb0c61e90b8f39
Rachael Graham <rachael.graham@solo.io>
2023-06-08 11:39:30 -0500

    test

--- codegen/doc/helm_values.go
+++ codegen/doc/helm_values.go
@@ -136,7 +136,9 @@ func docReflect(addValue addValue, path []string, desc string, typ reflect.Type,
 		path[lst] = path[lst] + "[]"
 
 		// add entry for slice field itself
-		addValue(HelmValue{Key: strings.Join(path, "."), Type: "[]" + typ.Elem().Kind().String(), DefaultValue: valToString(val), Description: desc})
+		if len(path) > 0 {
+			addValue(HelmValue{Key: strings.Join(path, "."), Type: "[]" + typ.Elem().Kind().String(), DefaultValue: valToString(val), Description: desc})
+		}
 
 		docReflect(addValue, path, desc, typ.Elem(), reflect.Value{})
 	case reflect.Struct:
