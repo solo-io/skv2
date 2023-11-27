c966995d8eaf2186bb50a22acd771acf136b7c44
Fabian Gonzalez <fabian.gonzalez@solo.io>
2023-11-27 12:22:36 -0500

    Add AppendingErrorHandler unit tests

--- contrib/pkg/output/errhandlers/err_handlers.go
+++ contrib/pkg/output/errhandlers/err_handlers.go
@@ -9,20 +9,30 @@ import (
 
 // this file contains ErrorHandlers for handling errors created when writing output snapshots
 
+func ResourceWriteError(resource ezkube.Object, err error) error {
+	return eris.Wrapf(err, "writing resource %v failed", sets.Key(resource))
+}
+func ResourceDeleteError(resource ezkube.Object, err error) error {
+	return eris.Wrapf(err, "deleting resource %v failed", sets.Key(resource))
+}
+func ListError(err error) error {
+	return eris.Wrapf(err, "listing failed")
+}
+
 type AppendingErrHandler struct {
 	errs error
 }
 
 func (a *AppendingErrHandler) HandleWriteError(resource ezkube.Object, err error) {
-	a.errs = multierror.Append(a.errs, eris.Wrapf(err, "writing resource %v failed", sets.Key(resource)))
+	a.errs = multierror.Append(a.errs, ResourceWriteError(resource, err))
 }
 
 func (a *AppendingErrHandler) HandleDeleteError(resource ezkube.Object, err error) {
-	a.errs = multierror.Append(a.errs, eris.Wrapf(err, "deleting resource %v failed", sets.Key(resource)))
+	a.errs = multierror.Append(a.errs, ResourceDeleteError(resource, err))
 }
 
 func (a *AppendingErrHandler) HandleListError(err error) {
-	a.errs = multierror.Append(a.errs, eris.Wrapf(err, "listing failed"))
+	a.errs = multierror.Append(a.errs, ListError(err))
 }
 
 // returns the errors collected by the handler
