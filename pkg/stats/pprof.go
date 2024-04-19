a4c9691607e93fee4a717bf77d6b4bc6a03950d8
Keerthan Ekbote <saiskee@gmail.com>
2024-04-19 09:19:28 -0400

    Add godeltapprof to debug pprof endpoints (#547)
    * add delta pprof
    
    * go mod tidy
    
    * goimports
    
    * codegen

--- pkg/stats/pprof.go
+++ pkg/stats/pprof.go
@@ -3,6 +3,8 @@ package stats
 import (
 	"net/http"
 	"net/http/pprof"
+
+	deltapprof "github.com/grafana/pyroscope-go/godeltaprof/http/pprof"
 )
 
 func AddPprof(mux *http.ServeMux) {
@@ -11,6 +13,9 @@ func AddPprof(mux *http.ServeMux) {
 	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
 	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
 	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	mux.HandleFunc("/debug/pprof/delta_heap", deltapprof.Heap)
+	mux.HandleFunc("/debug/pprof/delta_threadcreate", deltapprof.Mutex)
+	mux.HandleFunc("/debug/pprof/delta_block", deltapprof.Block)
 
 	profileDescriptions["/debug/pprof/"] = `PProf related things:<br/>
 	<a href="/debug/pprof/goroutine?debug=2">full goroutine stack dump</a>
